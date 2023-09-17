dd7dd20f5321afe50851917a8cf38fe3a65ea9e0
Balasubramani <ebalasubramani890@gmail.com>
2023-09-17 09:30:31 +0530

    Login API completed

--- budgettrackapi/src/main/java/com/budgettrack/budgettrackapi/Service/RegistrationServiceImpl.java
+++ budgettrackapi/src/main/java/com/budgettrack/budgettrackapi/Service/RegistrationServiceImpl.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.budgettrack.budgettrackapi.DTO.LoginDTO;
 import com.budgettrack.budgettrackapi.DTO.RegistrationSaveDTO;
 import com.budgettrack.budgettrackapi.Entity.RegistrationEntity;
 import com.budgettrack.budgettrackapi.Repository.RegistrationRepo;
@@ -20,26 +21,47 @@ public class RegistrationServiceImpl implements RegistrationService {
 
 
 	@Override
-	public String addRegistration(RegistrationSaveDTO registrationSaveDTO) {
+	public boolean addRegistration(RegistrationSaveDTO registrationSaveDTO) {
 		
 		try {			
-		if (registrationRepo.findByMobileNumber(registrationSaveDTO.getCustomerMobile())) {
-			return "Mobile Number Already Exists!!";
-		}
-		else
-		{			
+		if (!registrationRepo.findByMobileNumber(registrationSaveDTO.getCustomerMobile())) {
 			RegistrationEntity registrationEntity = new RegistrationEntity();
 			registrationEntity.setRegisterDate(LocalDate.now());
 			registrationEntity.setCustomerName(registrationSaveDTO.getCustomerName());
 			registrationEntity.setCustomerMobile(registrationSaveDTO.getCustomerMobile());
 			registrationEntity.setPassword(registrationSaveDTO.getPassword());
 			registrationRepo.save(registrationEntity);
-			return "User Added Successfully!!";
+			return true;			
+		}
+		else
+		{			
+			return false;
 		}
 		}
 		catch(Exception e)
 		{
-			return "Kindly Check: " +e;
+			return false;
+		}
+	}
+	
+	@Override
+	public long loginService(LoginDTO loginDTO)
+	{
+		try {
+		if(registrationRepo.validLogin(loginDTO.getCustomerMobile(), loginDTO.getPassword()))
+		{
+			RegistrationEntity registrationEntity = new RegistrationEntity();
+			Long customerId =  registrationRepo.loginAccept(loginDTO.getCustomerMobile(), loginDTO.getPassword());
+			return customerId;
+		}
+		else
+		{
+			return 0;
+		}
+		}
+		catch(Exception e)
+		{
+			return -1L;
 		}
 	}
 
